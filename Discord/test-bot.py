fbf75affed66d981e2e62b449bca858aa8dcf207
antanaskubilius2 <70003998+antanaskubilius2@users.noreply.github.com>
2021-01-06 10:37:02 -0600

    Add files via upload

---
+++ Discord/test-bot.py
@@ -0,0 +1,22 @@
+  
+import discord
+
+client = discord.Client()
+
+
+@client.event
+
+async def on_message(message):
+    message.content = message.content.lower()
+    if message.author == client.user:
+        return
+    if message.content.startswith("!help"):
+
+        if str(message.author) == "TSZ_Tonez#6999":  # make sure to change to your user name with hash code
+            await message.channel.send("!help " + str(message.author) + "!")
+        else:
+            await message.channel.send("Hello, I am a test bot.")
+    
+async def on_ready():
+        await client.change_presence(status=discord.Status.do_not_disturb, activity=discord.Game('Fortnite'))
+client.run('Njk1MTAzODQ3MDM0NTE5NTUy.XoVT5w.QUOnHqWId4b_qHaWL1Z2U-o4JYc')  # copy your token from the developer portal
